1431bb872b4b4ba63cb83396ffd004ed0e4e5066
William Dias <williamdiasvargas@gmail.com>
2017-04-26 01:59:18 -0400

    let e const capitulo 8

---
+++ chapter8/diferenca-uso-let_const.js
@@ -0,0 +1,43 @@
+/**
+ * Created by williamdias on 26/04/17.
+ */
+
+var mensagem = 'ola';
+{
+    var mensagem = 'adeus';
+}
+console.log(mensagem);
+
+var mensagem = 'Ola';
+
+function mensagem() {
+    var mensagem = 'adeus';
+}
+
+
+
+const arrayVar = [];
+
+for (var i = 1; i < 5; i++){
+    ArrayVar.push(function () {
+        console.log(i)
+    });
+}
+
+
+const arrayLet = [];
+
+for (let i = 1; i < 5; i++){
+    arrayLet.push(function () {
+        console.log(i)
+    })
+}
+
+ArrayVar.forEach(function (funcao) {
+    funcao();
+});
+
+arrayLet.forEach(function (funcao) {
+    funcao();
+});
+
---
+++ chapter8/let_const.js
@@ -0,0 +1,38 @@
+/**
+ * Created by williamdias on 26/04/17.
+ */
+
+var objeto = {};
+var numero = 1;
+var nome = "Chaves";
+var lista = [1,2,3];
+
+
+var texto = 'texto';
+var idade = 10;
+
+console.log(texto + idade);
+
+//const para valores q nao se alteram
+
+const dataNascimento = '19/10/1993';
+//dataNascimento = '25/08/1996';
+
+
+const pessoa = {nome : 'joao'};
+pessoa.idade = 12;
+//pessoa = novaPessoa;
+console.log(pessoa)
+
+function soma(a,b) {
+    return a + b
+}
+
+let resultado = 0;
+resultado = soma(2,3);
+console.log(resultado);
+
+var id = 1;
+var id = 2;
+console.log(id);
+
